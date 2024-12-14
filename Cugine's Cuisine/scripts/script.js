958b32674bb52abfd37c9e515b917ef3602f4eb3
AntPrato <prato.a@northeastern.edu>
2024-12-14 14:54:05 -0500

    Add files via upload

---
+++ Cugine's Cuisine/scripts/script.js
@@ -0,0 +1,3 @@
+// JavaScript Document
+
+alert('Thank you for viewing our website! We hope you come join us for dinner sometime! Continue forward if you want to place a pickup or delivery order! Buon appetito!')
---
+++ Cugine's Cuisine/scripts/script2.js
@@ -0,0 +1,4 @@
+// JavaScript Document
+
+alert('Read about our reasoning behind starting this restaurant and why we have so much passion behind everything we prepare!')
+	  
